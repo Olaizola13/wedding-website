e90d94a813c25fbd1a02b710079ed32838a0a438
Juanma Olaizola <39867258+Olaizola13@users.noreply.github.com>
2025-08-11 23:19:10 +0200

    Create recommendations.js

---
+++ js/translations/recommendations.js
@@ -0,0 +1,9 @@
+Object.assign(translations.en, {
+    "recommendations_page_title": "Recommendations"
+});
+Object.assign(translations.es, {
+    "recommendations_page_title": "Recomendaciones"
+});
+Object.assign(translations.de, {
+    "recommendations_page_title": "Sehenswertes"
+});
