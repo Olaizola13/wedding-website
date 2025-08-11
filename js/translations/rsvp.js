380c18c7527c63193c37d21ea3b0107db940050c
Juanma Olaizola <39867258+Olaizola13@users.noreply.github.com>
2025-08-11 23:09:00 +0200

    Create rsvp.js

---
+++ js/translations/rsvp.js
@@ -0,0 +1,24 @@
+Object.assign(translations.en, {
+    "rsvp_page_title": "RSVP",
+    "thank_you_title": "Thank You!",
+    "thank_you_message": "Thank you so much for your response! We're so excited to celebrate our big day with our favorite people. For those who can make it, we are counting down the days! For those who can't, please know you'll be in our hearts and we'll miss you.",
+    "add_to_calendar_title": "Attending? Add the Dates!",
+    "add_prewedding_btn": "Add Get-Together to Calendar",
+    "add_wedding_btn": "Add Wedding to Calendar"
+});
+Object.assign(translations.es, {
+    "rsvp_page_title": "RSVP",
+    "thank_you_title": "¡Gracias!",
+    "thank_you_message": "¡Muchas gracias por tu respuesta! Estamos muy emocionados de celebrar nuestro gran día con nuestra gente querida. Para los que podáis venir, ¡ya estamos contando los días! Para los que no, que sepáis que estaréis en nuestros corazones y os echaremos de menos.",
+    "add_to_calendar_title": "¿Vienes? ¡Apunta las Fechas!",
+    "add_prewedding_btn": "Añadir Preboda al Calendario",
+    "add_wedding_btn": "Añadir Boda al Calendario"
+});
+Object.assign(translations.de, {
+    "rsvp_page_title": "RSVP",
+    "thank_you_title": "Danke!",
+    "thank_you_message": "Vielen Dank für deine Rückmeldung! Wir freuen uns riesig darauf, unseren großen Tag mit unseren Lieblingsmenschen zu feiern. Für diejenigen, die dabei sein können: Wir zählen schon die Tage! Für diejenigen, die nicht kommen können: Ihr werdet in unseren Herzen sein und wir werden euch sehr vermissen.",
+    "add_to_calendar_title": "Du kommst? Speicher die Termine!",
+    "add_prewedding_btn": "Get-Together zum Kalender hinzufügen",
+    "add_wedding_btn": "Hochzeit zum Kalender hinzufügen"
+});
