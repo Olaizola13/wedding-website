b99b39b35c0fd12062e3599aa83117892d9390bf
Juanma Olaizola <39867258+Olaizola13@users.noreply.github.com>
2025-08-11 23:18:24 +0200

    Create kids.js

---
+++ js/translations/kids.js
@@ -0,0 +1,9 @@
+Object.assign(translations.en, {
+    "kids_page_title": "Kids"
+});
+Object.assign(translations.es, {
+    "kids_page_title": "Niños"
+});
+Object.assign(translations.de, {
+    "kids_page_title": "Kinder"
+});
