bc51794463417a1de38e215a2d53648c81a3f3bd
Juanma Olaizola <39867258+Olaizola13@users.noreply.github.com>
2025-08-12 00:13:21 +0200

    Update accomodations.js

--- js/translations/accomodations.js
+++ js/translations/accomodations.js
@@ -1,9 +1,24 @@
 Object.assign(translations.en, {
-    "accommodations_page_title": "Accommodation"
+    "accommodations_page_title": "Accommodation",
+    "accommodations_intro": "We highly recommend our guests stay in Valladolid. It's Juanma's beautiful hometown and the city that has the most to offer in terms of culture, food, and atmosphere. The Get-Together on Friday will also be held here.",
+    "accommodations_booking_title": "Hotel Recommendations",
+    "accommodations_booking_text": "We have been in touch with a variety of hotels located in the city center and near the train station to suit different preferences and budgets. The full list of recommendations will be provided here shortly. Please feel free to book whatever works best for you.",
+    "accommodations_warning_title": "Important: Book in Advance!",
+    "accommodations_warning_text": "Please keep in mind that our wedding weekend coincides with 'Seminci', the Valladolid International Film Festival. The city is very popular in October and attracts a lot of tourism, so we strongly advise you to book your accommodation well in advance to ensure availability and better prices."
 });
 Object.assign(translations.es, {
-    "accommodations_page_title": "Alojamiento"
+    "accommodations_page_title": "Alojamiento",
+    "accommodations_intro": "Recomendamos encarecidamente a nuestros invitados que se alojen en Valladolid. Es la preciosa ciudad natal de Juanma y la que más tiene que ofrecer en cuanto a cultura, gastronomía y ambiente. La preboda del viernes también se celebrará aquí.",
+    "accommodations_booking_title": "Hoteles Recomendados",
+    "accommodations_booking_text": "Hemos contactado con una variedad de hoteles situados en el centro de la ciudad y cerca de la estación de tren para adaptarnos a diferentes preferencias y presupuestos. La lista completa de recomendaciones se publicará aquí en breve. Por favor, sentíos libres de reservar lo que mejor os convenga.",
+    "accommodations_warning_title": "Importante: ¡Reservad con Antelación!",
+    "accommodations_warning_text": "Por favor, tened en cuenta que el fin de semana de nuestra boda coincide con la 'Seminci', el Festival Internacional de Cine de Valladolid. La ciudad es muy popular en octubre y atrae mucho turismo, por lo que os aconsejamos encarecidamente que reservéis vuestro alojamiento con bastante antelación para asegurar disponibilidad y mejores precios."
 });
 Object.assign(translations.de, {
-    "accommodations_page_title": "Unterkünfte"
+    "accommodations_page_title": "Unterkünfte",
+    "accommodations_intro": "Wir empfehlen unseren Gästen dringend, in Valladolid zu übernachten. Es ist Juanmas wunderschöne Heimatstadt und die Stadt, die in Bezug auf Kultur, Essen und Atmosphäre am meisten zu bieten hat. Das Get-Together am Freitag wird ebenfalls hier stattfinden.",
+    "accommodations_booking_title": "Hotel-Empfehlungen",
+    "accommodations_booking_text": "Wir haben mit einer Vielzahl von Hotels im Stadtzentrum und in der Nähe des Bahnhofs gesprochen, um verschiedenen Vorlieben und Budgets gerecht zu werden. Die vollständige Liste der Empfehlungen wird in Kürze hier veröffentlicht. Bitte fühlt euch frei, das zu buchen, was für euch am besten passt.",
+    "accommodations_warning_title": "Wichtig: Frühzeitig buchen!",
+    "accommodations_warning_text": "Bitte beachtet, dass unser Hochzeitswochenende mit der 'Seminci', dem Internationalen Filmfestival von Valladolid, zusammenfällt. Die Stadt ist im Oktober sehr beliebt und zieht viele Touristen an, daher raten wir euch dringend, eure Unterkunft weit im Voraus zu buchen, um Verfügbarkeit und bessere Preise zu gewährleisten."
 });
