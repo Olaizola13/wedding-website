1490c60e8cdee6448ba9554bbe3909210d1879fe
Juanma Olaizola <39867258+Olaizola13@users.noreply.github.com>
2025-08-11 19:46:14 +0200

    Update translations.js

--- js/translations.js
+++ js/translations.js
@@ -45,7 +45,7 @@ const translations = {
         "kids_page_title": "Kids",
         "transport_page_title": "Transport",
         "recommendations_page_title": "Recommendations",
-        "gifts_page_title": "Wedding Gifts",
+        "gifts_page_title": "Gifts", // UPDATED
         "gifts_message_intro": "Your presence at our wedding is the greatest gift we could ever ask for. Sharing this special day with you, our family and friends, is what matters most to us.",
         "gifts_message_main": "Many of you have asked about a gift list. As we have all the household items we need, we haven't created a traditional registry. However, if you would like to give a gift, a contribution towards our future life together would be sincerely appreciated.",
         "gifts_bank_details_title": "Bank Information",
@@ -98,7 +98,7 @@ const translations = {
         "kids_page_title": "Niños",
         "transport_page_title": "Transporte",
         "recommendations_page_title": "Recomendaciones",
-        "gifts_page_title": "Regalo de Boda",
+        "gifts_page_title": "Regalos", // UPDATED
         "gifts_message_intro": "Vuestra presencia en nuestra boda es el mejor regalo que podríamos pedir. Compartir este día tan especial con vosotros, nuestra familia y amigos, es lo que más nos importa.",
         "gifts_message_main": "Muchos de vosotros nos habéis preguntado por la lista de bodas. Como ya tenemos todo lo que necesitamos para nuestro hogar, no hemos creado una lista de regalos tradicional. Sin embargo, si deseáis hacernos un obsequio, una contribución para nuestra futura vida juntos sería increíblemente agradecida.",
         "gifts_bank_details_title": "Información Bancaria",
@@ -151,7 +151,7 @@ const translations = {
         "kids_page_title": "Kinder",
         "transport_page_title": "Transport",
         "recommendations_page_title": "Sehenswertes",
-        "gifts_page_title": "Hochzeitsgeschenke",
+        "gifts_page_title": "Geschenke", // UPDATED
         "gifts_message_intro": "Eure Anwesenheit bei unserer Hochzeit ist das größte Geschenk, das wir uns wünschen können. Diesen besonderen Tag mit euch, unserer Familie und unseren Freunden zu teilen, ist das, was uns am meisten bedeutet.",
         "gifts_message_main": "Viele von euch haben nach einer Geschenkeliste gefragt. Da wir bereits alle Haushaltsgegenstände haben, die wir brauchen, haben wir keine traditionelle Geschenkeliste erstellt. Solltet ihr uns dennoch eine Freude machen wollen, würden wir uns über einen Beitrag für unsere gemeinsame Zukunft von Herzen freuen.",
         "gifts_bank_details_title": "Bankverbindung",
