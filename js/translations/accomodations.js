a6c8b1726d05028e581e817a518627f79f4b2d7a
Juanma Olaizola <39867258+Olaizola13@users.noreply.github.com>
2025-08-11 23:17:57 +0200

    Create accomodations.js

---
+++ js/translations/accomodations.js
@@ -0,0 +1,9 @@
+Object.assign(translations.en, {
+    "accommodations_page_title": "Accommodation"
+});
+Object.assign(translations.es, {
+    "accommodations_page_title": "Alojamiento"
+});
+Object.assign(translations.de, {
+    "accommodations_page_title": "Unterkünfte"
+});
