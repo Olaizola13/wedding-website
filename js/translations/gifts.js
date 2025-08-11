9e1371caaf504ea16bc1cd6695533b049e681669
Juanma Olaizola <39867258+Olaizola13@users.noreply.github.com>
2025-08-11 23:18:53 +0200

    Create gifts.js

---
+++ js/translations/gifts.js
@@ -0,0 +1,24 @@
+Object.assign(translations.en, {
+    "gifts_page_title": "Gifts",
+    "gifts_message_intro": "Your presence at our wedding is the greatest gift we could ever ask for. Sharing this special day with you, our family and friends, is what matters most to us.",
+    "gifts_message_main": "Many of you have asked about a gift list. As we have all the household items we need, we haven't created a traditional registry. However, if you would like to give a gift, a contribution towards our future life together would be sincerely appreciated.",
+    "gifts_bank_details_title": "Bank Information",
+    "gifts_bank_holder": "Account Holder",
+    "gifts_thank_you": "Thank you so much for your love and generosity!"
+});
+Object.assign(translations.es, {
+    "gifts_page_title": "Regalos",
+    "gifts_message_intro": "Vuestra presencia en nuestra boda es el mejor regalo que podríamos pedir. Compartir este día tan especial con vosotros, nuestra familia y amigos, es lo que más nos importa.",
+    "gifts_message_main": "Muchos de vosotros nos habéis preguntado por la lista de bodas. Como ya tenemos todo lo que necesitamos para nuestro hogar, no hemos creado una lista de regalos tradicional. Sin embargo, si deseáis hacernos un obsequio, una contribución para nuestra futura vida juntos sería increíblemente agradecida.",
+    "gifts_bank_details_title": "Información Bancaria",
+    "gifts_bank_holder": "Titular de la Cuenta",
+    "gifts_thank_you": "¡Muchísimas gracias por vuestro cariño y generosidad!"
+});
+Object.assign(translations.de, {
+    "gifts_page_title": "Geschenke",
+    "gifts_message_intro": "Eure Anwesenheit bei unserer Hochzeit ist das größte Geschenk, das wir uns wünschen können. Diesen besonderen Tag mit euch, unserer Familie und unseren Freunden zu teilen, ist das, was uns am meisten bedeutet.",
+    "gifts_message_main": "Viele von euch haben nach einer Geschenkeliste gefragt. Da wir bereits alle Haushaltsgegenstände haben, die wir brauchen, haben wir keine traditionelle Geschenkeliste erstellt. Solltet ihr uns dennoch eine Freude machen wollen, würden wir uns über einen Beitrag für unsere gemeinsame Zukunft von Herzen freuen.",
+    "gifts_bank_details_title": "Bankverbindung",
+    "gifts_bank_holder": "Kontoinhaber",
+    "gifts_thank_you": "Vielen herzlichen Dank für eure Liebe und Großzügigkeit!"
+});
