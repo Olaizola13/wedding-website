11e162b3e2e48fda19596b04db6451964e325e20
Juanma Olaizola <39867258+Olaizola13@users.noreply.github.com>
2025-08-11 23:08:43 +0200

    Create wedding.js

---
+++ js/translations/wedding.js
@@ -0,0 +1,36 @@
+Object.assign(translations.en, {
+    "date": "17 . 10 . 2026",
+    "dear_guests": "Dear Guests,",
+    "pre_wedding_title": "Get-Together",
+    "pre_wedding_date": "<strong>When:</strong> Friday, 16.10.2026, from 7:00 PM",
+    "pre_wedding_location": "<strong>Where:</strong> Location in Valladolid (TBD)",
+    "pre_wedding_dresscode": "<strong>Dresscode:</strong> Casual Chic",
+    "wedding_title": "The Wedding",
+    "wedding_date_long": "<strong>When:</strong> Saturday, 17.10.2026, from 1:00 PM",
+    "wedding_location": "<strong>Where:</strong> Posada Real del Pinar",
+    "wedding_dresscode": "<strong>Dresscode:</strong> Formal Attire"
+});
+Object.assign(translations.es, {
+    "date": "17 . 10 . 2026",
+    "dear_guests": "Queridos Invitados,",
+    "pre_wedding_title": "Preboda",
+    "pre_wedding_date": "<strong>Cuándo:</strong> Viernes, 16.10.2026, desde las 19:00h",
+    "pre_wedding_location": "<strong>Dónde:</strong> Lugar en Valladolid (pendiente)",
+    "pre_wedding_dresscode": "<strong>Etiqueta:</strong> Casual Chic",
+    "wedding_title": "La Boda",
+    "wedding_date_long": "<strong>Cuándo:</strong> Sábado, 17.10.2026, desde las 13:00h",
+    "wedding_location": "<strong>Dónde:</strong> Posada Real del Pinar",
+    "wedding_dresscode": "<strong>Etiqueta:</strong> Formal"
+});
+Object.assign(translations.de, {
+    "date": "17 . 10 . 2026",
+    "dear_guests": "Liebe Gäste,",
+    "pre_wedding_title": "Get-Together",
+    "pre_wedding_date": "<strong>Wann:</strong> Freitag, 16.10.2026, ab 19:00 Uhr",
+    "pre_wedding_location": "<strong>Wo:</strong> Ort in Valladolid (wird noch bekannt gegeben)",
+    "pre_wedding_dresscode": "<strong>Dresscode:</strong> Casual Chic",
+    "wedding_title": "Die Hochzeit",
+    "wedding_date_long": "<strong>Wann:</strong> Samstag, 17.10.2026, ab 13:00 Uhr",
+    "wedding_location": "<strong>Wo:</strong> Posada Real del Pinar",
+    "wedding_dresscode": "<strong>Dresscode:</strong> Festliche Kleidung"
+});
