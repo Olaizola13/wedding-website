dc862f12fba1fd9690762bb519b881d81b7759a5
Juanma Olaizola <39867258+Olaizola13@users.noreply.github.com>
2025-08-11 23:19:24 +0200

    Create memories.js

---
+++ js/translations/memories.js
@@ -0,0 +1,18 @@
+Object.assign(translations.en, {
+    "memories_page_title": "Share Your Memories",
+    "memories_intro": "One of the greatest gifts you can give us is to share the wonderful moments you capture during our celebration! We've created a shared album where you can easily upload your photos and videos from both the Get-Together and the Wedding day.",
+    "memories_cta_button": "Go to Upload Album",
+    "memories_thanks": "We can't wait to see the day through your eyes!"
+});
+Object.assign(translations.es, {
+    "memories_page_title": "Comparte Tus Recuerdos",
+    "memories_intro": "¡Uno de los mejores regalos que podéis darnos es compartir los maravillosos momentos que capturéis durante nuestra celebración! Hemos creado un álbum compartido donde podéis subir fácilmente vuestras fotos y vídeos tanto de la preboda como del día de la boda.",
+    "memories_cta_button": "Ir al Álbum de Carga",
+    "memories_thanks": "¡Estamos deseando ver el día a través de vuestros ojos!"
+});
+Object.assign(translations.de, {
+    "memories_page_title": "Teile deine Erinnerungen",
+    "memories_intro": "Eines der größten Geschenke, die ihr uns machen könnt, ist, die wundervollen Momente zu teilen, die ihr während unserer Feier festhaltet! Wir haben ein gemeinsames Album erstellt, in das ihr ganz einfach eure Fotos und Videos vom Get-Together und vom Hochzeitstag hochladen könnt.",
+    "memories_cta_button": "Zum Upload-Album",
+    "memories_thanks": "Wir können es kaum erwarten, den Tag durch eure Augen zu sehen!"
+});
