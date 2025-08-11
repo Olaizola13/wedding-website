7c2ea0ee731155e772213b485abdac9feb3b0444
Juanma Olaizola <39867258+Olaizola13@users.noreply.github.com>
2025-08-11 23:18:38 +0200

    Create transport.js

---
+++ js/translations/transport.js
@@ -0,0 +1,9 @@
+Object.assign(translations.en, {
+    "transport_page_title": "Transport"
+});
+Object.assign(translations.es, {
+    "transport_page_title": "Transporte"
+});
+Object.assign(translations.de, {
+    "transport_page_title": "Transport"
+});
