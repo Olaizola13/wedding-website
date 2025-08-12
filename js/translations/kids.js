19d64a193fb6f49071853228b7ff88057789f420
Juanma Olaizola <39867258+Olaizola13@users.noreply.github.com>
2025-08-12 18:08:55 +0200

    Update kids.js

--- js/translations/kids.js
+++ js/translations/kids.js
@@ -1,9 +1,27 @@
 Object.assign(translations.en, {
-    "kids_page_title": "Kids"
+    "kids_page_title": "Kids",
+    "kids_intro_1": "Our daughter, Nuria, will of course be celebrating with us, and it wouldn't be a party without her friends!",
+    "kids_intro_2": "Many of our friends are parents, and your children are a special part of your lives, so they are a special part of ours too. Children are more than welcome to join in on all the celebrations.",
+    "kids_at_the_venue_title": "On the Wedding Day",
+    "kids_at_the_venue_text": "The wedding venue, Posada Real del Pinar, is a wonderful place for children. It has a small playground and plenty of safe, open space for them to run around and play. A must-see for them will be the resident donkey and the beautiful peacocks that also live on the estate!",
+    "kids_planning_title": "A Little Help for Our Planning",
+    "kids_planning_text": "So that we can plan a bit of entertainment accordingly on the wedding day, we would be very grateful if you could let us know if you'll be bringing your children along when you fill out the RSVP. More details on the entertainment will be shared here closer to the date!"
 });
 Object.assign(translations.es, {
-    "kids_page_title": "Niños"
+    "kids_page_title": "Niños",
+    "kids_intro_1": "Nuestra hija, Nuria, por supuesto celebrará con nosotros, ¡y no sería una fiesta sin sus amigos!",
+    "kids_intro_2": "Muchos de nuestros amigos sois padres, y vuestros hijos son una parte especial de vuestras vidas, así que también lo son de la nuestra. Los niños son más que bienvenidos a unirse a todas las celebraciones.",
+    "kids_at_the_venue_title": "El Día de la Boda",
+    "kids_at_the_venue_text": "El lugar de la boda, la Posada Real del Pinar, es un sitio maravilloso para los niños. Tiene un pequeño parque infantil y mucho espacio abierto y seguro para que corran y jueguen. ¡Una visita obligada para ellos será el burro residente y los preciosos pavos reales que también viven en la finca!",
+    "kids_planning_title": "Una Pequeña Ayuda para Planificar",
+    "kids_planning_text": "Para que podamos planificar un poco de entretenimiento para los más pequeños el día de la boda, os agradeceríamos mucho que nos hicierais saber si traeréis a vuestros hijos cuando rellenéis la confirmación de asistencia. ¡Se compartirán más detalles sobre el entretenimiento aquí más cerca de la fecha!"
 });
 Object.assign(translations.de, {
-    "kids_page_title": "Kinder"
+    "kids_page_title": "Kinder",
+    "kids_intro_1": "Unsere Tochter Nuria wird natürlich mit uns feiern, und es wäre keine Party ohne ihre Freunde!",
+    "kids_intro_2": "Viele unserer Freunde sind Eltern, und eure Kinder sind ein besonderer Teil eures Lebens, also sind sie auch ein besonderer Teil unseres. Kinder sind herzlich eingeladen, an allen Feierlichkeiten teilzunehmen.",
+    "kids_at_the_venue_title": "Am Hochzeitstag",
+    "kids_at_the_venue_text": "Der Hochzeitsort, Posada Real del Pinar, ist ein wunderbarer Ort für Kinder. Es gibt einen kleinen Spielplatz und viel sicheren, offenen Platz zum Herumlaufen und Spielen. Ein Muss für sie werden der ansässige Esel und die wunderschönen Pfauen sein, die ebenfalls auf dem Anwesen leben!",
+    "kids_planning_title": "Eine kleine Hilfe für unsere Planung",
+    "kids_planning_text": "Damit wir am Hochzeitstag für ein wenig Unterhaltung für die Kleinen sorgen können, wären wir euch sehr dankbar, wenn ihr uns beim Ausfüllen der RSVP mitteilen könntet, ob ihr eure Kinder mitbringt. Weitere Details zur Unterhaltung werden hier zu einem späteren Zeitpunkt bekannt gegeben!"
 });
