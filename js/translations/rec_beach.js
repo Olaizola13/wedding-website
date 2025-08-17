a65f6eb8c4eb97599b9ee0ea03274c5f7884e99e
Juanma Olaizola <39867258+Olaizola13@users.noreply.github.com>
2025-08-17 17:20:15 +0200

    Update rec_beach.js

--- js/translations/rec_beach.js
+++ js/translations/rec_beach.js
@@ -1,12 +1,12 @@
 Object.assign(translations.en, {
-    "rec_beach_page_title": "Little Caribbean", // UPDATED
-    "recommendations_beach_text": "Yes, Valladolid has a beach! Is it the Caribbean? No. Is it on the sea? Also no. But our mighty Pisuerga river provides a magnificent sandy stretch perfect for a sunny day. If you close your eyes *really* tight and ignore the 16th-century buildings on the other side, it's basically the same thing. Almost."
+    "rec_beach_page_title": "Little Caribbean",
+    "recommendations_beach_text": "Yes, Valladolid has a beach! Is it the Caribbean? No. Is it on the sea? Also no. But our mighty Pisuerga river provides a magnificent sandy stretch perfect for a sunny day. If you squint your eyes *really* hard, ignore the 12-story buildings on the other side and order a mojito, it's basically the same thing. Almost."
 });
 Object.assign(translations.es, {
-    "rec_beach_page_title": "El Pequeño Caribe", // UPDATED
-    "recommendations_beach_text": "¡Así es, tenemos playa! ¿Es el Caribe? No. ¿Está en el mar? Tampoco. Pero nuestro caudaloso río Pisuerga nos regala una magnífica franja de arena perfecta para un día de sol. Si cierras los ojos *muy* fuerte e ignoras los edificios del siglo XVI de la otra orilla, es básicamente lo mismo. Casi."
+    "rec_beach_page_title": "El Pequeño Caribe",
+    "recommendations_beach_text": "¡Sí, Valladolid tiene playa! ¿Es el Caribe? No. ¿Está en el mar? Tampoco. Pero nuestro caudaloso río Pisuerga nos regala una magnífica franja de arena perfecta para un día de sol. Si entrecierras los ojos *muy* fuerte, ignoras los edificios de 12 plantas de la otra orilla y pides un mojito, es básicamente lo mismo. Casi."
 });
 Object.assign(translations.de, {
-    "rec_beach_page_title": "Kleine Karibik", // UPDATED
-    "recommendations_beach_text": "Richtig gehört, wir haben einen Strand. Ist es die Karibik? Nein. Liegt er am Meer? Auch nicht. Aber unser mächtiger Fluss Pisuerga beschert uns einen herrlichen Sandstreifen, perfekt für einen sonnigen Tag. Wenn man die Augen *ganz* fest schließt und die Gebäude aus dem 16. Jahrhundert auf der anderen Seite ignoriert, ist es im Grunde dasselbe. Fast."
+    "rec_beach_page_title": "Kleine Karibik",
+    "recommendations_beach_text": "Ja, Valladolid hat einen Strand! Ist es die Karibik? Nein. Liegt er am Meer? Auch nicht. Aber unser mächtiger Fluss Pisuerga beschert uns einen herrlichen Sandstrand, perfekt für einen sonnigen Tag. Wenn man die Augen *ganz* fest zusammenkneift, die 12-stöckigen Gebäude auf der anderen Seite ignoriert und einen Mojito bestellt, ist es im Grunde dasselbe. Fast."
 });
