273378e8e8cbf0e3f3094c0e83702be66f32652b
Juanma Olaizola <39867258+Olaizola13@users.noreply.github.com>
2025-08-17 16:49:49 +0200

    Update recommendations.js

--- js/translations/recommendations.js
+++ js/translations/recommendations.js
@@ -4,7 +4,7 @@ Object.assign(translations.en, {
     "recommendations_intro_text": "Valladolid and the wider region of Castile and León are full of history, incredible food, and wonderful sights. Below is an index to help you explore some of our favorite things about the area.",
     "rec_index_history": "History & Culture",
     "rec_index_cathedral": "The Cathedral",
-    "rec_index_beach": "The River Beach",
+    "rec_index_beach": "Little Caribbean", // UPDATED
     "rec_index_storks": "The Storks",
     "rec_index_park": "Campo Grande",
     "rec_index_dayout": "Our Day Out"
@@ -15,7 +15,7 @@ Object.assign(translations.es, {
     "recommendations_intro_text": "Valladolid y la extensa región de Castilla y León están llenas de historia, una gastronomía increíble y lugares maravillosos. A continuación, un índice para ayudaros a explorar algunas de nuestras cosas favoritas de la zona.",
     "rec_index_history": "Historia y Cultura",
     "rec_index_cathedral": "La Catedral",
-    "rec_index_beach": "La Playa Fluvial",
+    "rec_index_beach": "El Pequeño Caribe", // UPDATED
     "rec_index_storks": "Las Cigüeñas",
     "rec_index_park": "Campo Grande",
     "rec_index_dayout": "Nuestro Día Perfecto"
@@ -26,7 +26,7 @@ Object.assign(translations.de, {
     "recommendations_intro_text": "Valladolid und die weitere Region Kastilien und León sind reich an Geschichte, unglaublichem Essen und wunderschönen Sehenswürdigkeiten. Unten findet ihr ein Verzeichnis, das euch hilft, einige unserer Lieblingsdinge in der Gegend zu erkunden.",
     "rec_index_history": "Geschichte & Kultur",
     "rec_index_cathedral": "Die Kathedrale",
-    "rec_index_beach": "Der Flussstrand",
+    "rec_index_beach": "Kleine Karibik", // UPDATED
     "rec_index_storks": "Die Störche",
     "rec_index_park": "Campo Grande",
     "rec_index_dayout": "Unser perfekter Tag"
