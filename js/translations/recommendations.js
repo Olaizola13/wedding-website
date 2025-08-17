6d50a3144c7668f6c5e554697af99b8df6e1edc5
Juanma Olaizola <39867258+Olaizola13@users.noreply.github.com>
2025-08-17 16:32:21 +0200

    Update recommendations.js

--- js/translations/recommendations.js
+++ js/translations/recommendations.js
@@ -1,6 +1,7 @@
 Object.assign(translations.en, {
     "recommendations_page_title": "Recommendations",
     "recommendations_section1_title": "What Valladolid Has to Offer",
+    "recommendations_intro_text": "Valladolid and the wider region of Castile and León are full of history, incredible food, and wonderful sights. Below is an index to help you explore some of our favorite things about the area.",
     "rec_index_history": "History & Culture",
     "rec_index_cathedral": "The Cathedral",
     "rec_index_beach": "The River Beach",
@@ -11,6 +12,7 @@ Object.assign(translations.en, {
 Object.assign(translations.es, {
     "recommendations_page_title": "Recomendaciones",
     "recommendations_section1_title": "Qué Ofrece Valladolid",
+    "recommendations_intro_text": "Valladolid y la extensa región de Castilla y León están llenas de historia, una gastronomía increíble y lugares maravillosos. A continuación, un índice para ayudaros a explorar algunas de nuestras cosas favoritas de la zona.",
     "rec_index_history": "Historia y Cultura",
     "rec_index_cathedral": "La Catedral",
     "rec_index_beach": "La Playa Fluvial",
@@ -21,6 +23,7 @@ Object.assign(translations.es, {
 Object.assign(translations.de, {
     "recommendations_page_title": "Empfehlungen",
     "recommendations_section1_title": "Was Valladolid zu bieten hat",
+    "recommendations_intro_text": "Valladolid und die weitere Region Kastilien und León sind reich an Geschichte, unglaublichem Essen und wunderschönen Sehenswürdigkeiten. Unten findet ihr ein Verzeichnis, das euch hilft, einige unserer Lieblingsdinge in der Gegend zu erkunden.",
     "rec_index_history": "Geschichte & Kultur",
     "rec_index_cathedral": "Die Kathedrale",
     "rec_index_beach": "Der Flussstrand",
