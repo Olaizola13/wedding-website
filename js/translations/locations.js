188e16c5afeef7c3abf03f7036af7aa0cbccc91f
Juanma Olaizola <39867258+Olaizola13@users.noreply.github.com>
2025-08-17 17:39:17 +0200

    Update locations.js

--- js/translations/locations.js
+++ js/translations/locations.js
@@ -3,19 +3,25 @@ Object.assign(translations.en, {
     "locations_gettogether_title": "Friday: The Get-Together",
     "locations_gettogether_text": "The get-together will take place in Valladolid. It is a wonderful opportunity for all our guests, especially those coming from far away, to gather, connect, and get into the celebratory spirit with everyone before the big day. Finger food and some drinks will be provided. No means of transportation will be provided for this event. The exact location is to be determined and will be announced here soon!",
     "locations_wedding_title": "Saturday: The Wedding",
-    "locations_wedding_text": "The main event will take place at <strong>Posada Real del Pinar</strong>. Nestled in a beautiful pine forest, this historic Castilian estate offers a magical blend of rustic charm and natural elegance. It's a special place for us, and we can't wait to share its cozy atmosphere and beautiful surroundings with you as we say 'I do' and celebrate into the night."
+    "locations_wedding_text": "The main event will take place at <strong>Posada Real del Pinar</strong>. Nestled in a beautiful pine forest, this historic Castilian estate offers a magical blend of rustic charm and natural elegance. It's a special place for us, and we can't wait to share its cozy atmosphere and beautiful surroundings with you as we say 'I do' and celebrate into the night.",
+    "locations_wedding_address": "<strong>Address:</strong> Pinar de San Rafael, S/N, 47450 Pozal de Gallinas, Valladolid",
+    "locations_wedding_map_link": "View on Google Maps"
 });
 Object.assign(translations.es, {
     "locations_page_title": "Lugares",
     "locations_gettogether_title": "Viernes: La Preboda",
     "locations_gettogether_text": "La preboda tendrá lugar en Valladolid. Es una oportunidad maravillosa para que todos nuestros invitados, especialmente los que vienen de lejos, se reúnan, conecten y se sumerjan en el espíritu festivo con todos antes del gran día. Se ofrecerá picoteo y algunas bebidas. No se proporcionará transporte para este evento. ¡La ubicación exacta está por determinar y se anunciará aquí próximamente!",
     "locations_wedding_title": "Sábado: La Boda",
-    "locations_wedding_text": "El evento principal tendrá lugar en la <strong>Posada Real del Pinar</strong>. Ubicada en un precioso bosque de pinos, esta histórica finca castellana ofrece una mezcla mágica de encanto rústico y elegancia natural. Es un lugar especial para nosotros, y estamos deseando compartir su acogedor ambiente y sus hermosos alrededores con vosotros mientras nos damos el 'Sí, quiero' y celebramos hasta la noche."
+    "locations_wedding_text": "El evento principal tendrá lugar en la <strong>Posada Real del Pinar</strong>. Ubicada en un precioso bosque de pinos, esta histórica finca castellana ofrece una mezcla mágica de encanto rústico y elegancia natural. Es un lugar especial para nosotros, y estamos deseando compartir su acogedor ambiente y sus hermosos alrededores con vosotros mientras nos damos el 'Sí, quiero' y celebramos hasta la noche.",
+    "locations_wedding_address": "<strong>Dirección:</strong> Pinar de San Rafael, S/N, 47450 Pozal de Gallinas, Valladolid",
+    "locations_wedding_map_link": "Ver en Google Maps"
 });
 Object.assign(translations.de, {
     "locations_page_title": "Locations",
     "locations_gettogether_title": "Freitag: Das Get-Together",
     "locations_gettogether_text": "Das Get-Together findet in Valladolid statt. Es ist eine wunderbare Gelegenheit für all unsere Gäste, insbesondere für diejenigen, die von weither anreisen, sich zu treffen, auszutauschen und mit allen vor dem großen Tag in Feierlaune zu kommen. Fingerfood und einige Getränke werden bereitgestellt. Für diese Veranstaltung wird kein Transportmittel zur Verfügung gestellt. Der genaue Ort wird noch festgelegt und in Kürze hier bekannt gegeben!",
     "locations_wedding_title": "Samstag: Die Hochzeit",
-    "locations_wedding_text": "Die Hauptveranstaltung findet in der <strong>Posada Real del Pinar</strong> statt. In einem wunderschönen Pinienwald gelegen, bietet dieses historische kastilische Anwesen eine magische Mischung aus rustikalem Charme und natürlicher Eleganz. Es ist ein besonderer Ort für uns, und wir können es kaum erwarten, seine gemütliche Atmosphäre und die wunderschöne Umgebung mit euch zu teilen, während wir 'Ja' sagen und bis in die Nacht feiern."
+    "locations_wedding_text": "Die Hauptveranstaltung findet in der <strong>Posada Real del Pinar</strong> statt. In einem wunderschönen Pinienwald gelegen, bietet dieses historische kastilische Anwesen eine magische Mischung aus rustikalem Charme und natürlicher Eleganz. Es ist ein besonderer Ort für uns, und wir können es kaum erwarten, seine gemütliche Atmosphäre und die wunderschöne Umgebung mit euch zu teilen, während wir 'Ja' sagen und bis in die Nacht feiern.",
+    "locations_wedding_address": "<strong>Adresse:</strong> Pinar de San Rafael, S/N, 47450 Pozal de Gallinas, Valladolid",
+    "locations_wedding_map_link": "Auf Google Maps ansehen"
 });
