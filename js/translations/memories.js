4b81a86b8ed20e4adc046c5b27b4d96830fd8009
Juanma Olaizola <39867258+Olaizola13@users.noreply.github.com>
2025-08-18 12:58:08 +0200

    Update memories.js

--- js/translations/memories.js
+++ js/translations/memories.js
@@ -1,21 +1,24 @@
 Object.assign(translations.en, {
     "memories_page_title": "Share Your Memories",
-    "memories_intro": "One of the greatest gifts you can give us is to share the wonderful moments you capture during our celebration! We've created a shared album where you can easily upload your photos and videos from both the Get-Together and the Wedding day.",
-    "memories_note": "Please note: You'll need to be signed into a Google account to add your photos and videos to the album.",
-    "memories_cta_button": "Go to Upload Album",
+    "memories_intro": "One of the greatest gifts you can give us is to share the wonderful moments you capture! We've created shared albums where you can easily upload your photos and videos from both the Get-Together and the Wedding day.",
+    "memories_note": "Please choose the album that best suits your device.",
+    "memories_google_button": "Upload with Google Photos",
+    "memories_icloud_button": "Upload with iCloud (for iPhone)",
     "memories_thanks": "We can't wait to see the day through your eyes!"
 });
 Object.assign(translations.es, {
     "memories_page_title": "Comparte Tus Recuerdos",
-    "memories_intro": "¡Uno of the mejores regalos que podéis darnos es compartir los maravillosos momentos que capturéis durante nuestra celebración! Hemos creado un álbum compartido donde podéis subir fácilmente vuestras fotos y vídeos tanto de la preboda como del día de la boda.",
-    "memories_note": "Por favor, ten en cuenta que necesitarás iniciar sesión en una cuenta de Google para añadir tus fotos y vídeos al álbum.",
-    "memories_cta_button": "Ir al Álbum de Carga",
+    "memories_intro": "¡Uno de los mejores regalos que podéis darnos es compartir los maravillosos momentos que capturéis! Hemos creado álbumes compartidos donde podéis subir fácilmente vuestras fotos y vídeos tanto de la preboda como del día de la boda.",
+    "memories_note": "Por favor, elige el álbum que mejor se adapte a tu dispositivo.",
+    "memories_google_button": "Subir con Google Fotos",
+    "memories_icloud_button": "Subir con iCloud (para iPhone)",
     "memories_thanks": "¡Estamos deseando ver el día a través de vuestros ojos!"
 });
 Object.assign(translations.de, {
     "memories_page_title": "Teile deine Erinnerungen",
-    "memories_intro": "Eines der größten Geschenke, die ihr uns machen könnt, ist, die wundervollen Momente zu teilen, die ihr während unserer Feier festhaltet! Wir haben ein gemeinsames Album erstellt, in das ihr ganz einfach eure Fotos und Videos vom Get-Together und vom Hochzeitstag hochladen könnt.",
-    "memories_note": "Bitte beachte: Du musst in einem Google-Konto angemeldet sein, um deine Fotos und Videos zum Album hinzuzufügen.",
-    "memories_cta_button": "Zum Upload-Album",
+    "memories_intro": "Eines der größten Geschenke, die ihr uns machen könnt, ist, die wundervollen Momente zu teilen, die ihr festhaltet! Wir haben gemeinsame Alben erstellt, in die ihr ganz einfach eure Fotos und Videos vom Get-Together und vom Hochzeitstag hochladen könnt.",
+    "memories_note": "Bitte wähle das Album, das am besten zu deinem Gerät passt.",
+    "memories_google_button": "Mit Google Fotos hochladen",
+    "memories_icloud_button": "Mit iCloud hochladen (für iPhone)",
     "memories_thanks": "Wir können es kaum erwarten, den Tag durch eure Augen zu sehen!"
 });
