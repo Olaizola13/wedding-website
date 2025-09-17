3cf8e0266a1bba249e07d207c44881875aa69af4
Juanma Olaizola <39867258+Olaizola13@users.noreply.github.com>
2025-09-17 21:48:42 +0200

    Create song-requests.js

---
+++ js/translations/song-requests.js
@@ -0,0 +1,27 @@
+Object.assign(translations.en, {
+    "song_requests_page_title": "Song Requests",
+    "song_requests_intro": "Got a song that will get you on the dance floor? Let us know! We would love to include your favorite tunes in our party playlist.",
+    "form_label_name": "Your Name",
+    "form_label_artist": "Artist",
+    "form_label_song": "Song Title",
+    "form_submit_button": "Submit Request",
+    "form_success_message": "Thank you! Your song has been added to our list."
+});
+Object.assign(translations.es, {
+    "song_requests_page_title": "Peticiones Musicales",
+    "song_requests_intro": "¿Tienes una canción que te haga salir a la pista de baile? ¡Dínoslo! Nos encantaría incluir vuestras canciones favoritas en nuestra lista de reproducción.",
+    "form_label_name": "Tu Nombre",
+    "form_label_artist": "Artista",
+    "form_label_song": "Título de la Canción",
+    "form_submit_button": "Enviar Petición",
+    "form_success_message": "¡Gracias! Tu canción ha sido añadida a nuestra lista."
+});
+Object.assign(translations.de, {
+    "song_requests_page_title": "Musikwünsche",
+    "song_requests_intro": "Hast du einen Song, der dich auf die Tanzfläche lockt? Lass es uns wissen! Wir würden uns freuen, eure Lieblingslieder in unsere Party-Playlist aufzunehmen.",
+    "form_label_name": "Dein Name",
+    "form_label_artist": "Künstler",
+    "form_label_song": "Songtitel",
+    "form_submit_button": "Wunsch abschicken",
+    "form_success_message": "Vielen Dank! Dein Song wurde unserer Liste hinzugefügt."
+});
