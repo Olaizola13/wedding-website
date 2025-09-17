8a7fce01929997cd4767af938c51f286b62399bd
Juanma Olaizola <39867258+Olaizola13@users.noreply.github.com>
2025-09-17 20:56:15 +0200

    Update wedding.js

--- js/translations/wedding.js
+++ js/translations/wedding.js
@@ -1,7 +1,8 @@
 Object.assign(translations.en, {
     "date": "17 . 10 . 2026",
     "dear_guests": "Dear Guests,",
-    "event_intro": "Welcome to our wedding page. Here you can find all the information about our wedding event.",
+    "event_intro": "Welcome to our wedding page. Good that you are here! In this site you can find all the information about our wedding event.",
+    "event_intro_truncated": "Welcome to our wedding page. Good that you are here! In this site you can find . . .",
     "pre_wedding_title": "Get-Together",
     "pre_wedding_date": "<strong>When:</strong> Friday, 16.10.2026, from 7:00 PM",
     "pre_wedding_location": "<strong>Where:</strong> Location in Valladolid (TBD)",
@@ -15,7 +16,8 @@ Object.assign(translations.en, {
 Object.assign(translations.es, {
     "date": "17 . 10 . 2026",
     "dear_guests": "Queridos Invitados,",
-    "event_intro": "Bienvenidos a nuestra página de bodas. Aquí podéis encontrar toda la información sobre nuestro evento.",
+    "event_intro": "Bienvenidos a nuestra página de bodas. ¡Qué bien que estéis aquí! En esta web podéis encontrar toda la información sobre nuestro evento.",
+    "event_intro_truncated": "Bienvenidos a nuestra página de bodas. ¡Qué bien que estéis aquí! En esta web podéis encontrar . . .",
     "pre_wedding_title": "Preboda",
     "pre_wedding_date": "<strong>Cuándo:</strong> Viernes, 16.10.2026, desde las 19:00h",
     "pre_wedding_location": "<strong>Dónde:</strong> Lugar en Valladolid (pendiente)",
@@ -29,7 +31,8 @@ Object.assign(translations.es, {
 Object.assign(translations.de, {
     "date": "17 . 10 . 2026",
     "dear_guests": "Liebe Gäste,",
-    "event_intro": "Willkommen auf unserer Hochzeitsseite. Hier findet ihr alle Informationen rund um unsere Hochzeit.",
+    "event_intro": "Willkommen auf unserer Hochzeitsseite. Schön, dass ihr hier seid! Auf dieser Seite findet ihr alle Informationen zu unserer Hochzeit.",
+    "event_intro_truncated": "Willkommen auf unserer Hochzeitsseite. Schön, dass ihr hier seid! Auf dieser Seite findet ihr . . .",
     "pre_wedding_title": "Get-Together",
     "pre_wedding_date": "<strong>Wann:</strong> Freitag, 16.10.2026, ab 19:00 Uhr",
     "pre_wedding_location": "<strong>Wo:</strong> Ort in Valladolid (wird noch bekannt gegeben)",
