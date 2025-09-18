b0ee63a5b34f8dcff8331685f3ab3accb6828749
Juanma Olaizola <39867258+Olaizola13@users.noreply.github.com>
2025-09-18 20:17:46 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', () => {
+document.addEventListener('DOMContentLoaded', () => {
 
     const setLanguage = (language) => {
         const elements = document.querySelectorAll('[data-key]');
