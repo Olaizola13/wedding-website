3a61319e115e719b1324483118a0ad36681871e0
Juanma Olaizola <39867258+Olaizola13@users.noreply.github.com>
2025-09-18 22:15:41 +0200

    Update rsvp.js

--- js/translations/rsvp.js
+++ js/translations/rsvp.js
@@ -9,7 +9,7 @@ Object.assign(translations.en, {
 Object.assign(translations.es, {
     "rsvp_page_title": "RSVP",
     "thank_you_title": "¡Gracias!",
-    "thank_you_message": "¡Muchas gracias por tu respuesta! Estamos muy emocionados de celebrar nuestro gran día con nuestra gente querida. Para los que podáis venir, ¡ya estamos contando los días! Para los que no, que sepáis que estaréis en nuestros corazones y os echaremos de menos.",
+    "thank_you_message": "¡Muchas gracias por vuestra respuesta! Estamos muy emocionados de celebrar nuestro gran día con nuestra gente querida. Para los que podáis venir, ¡ya estamos contando los días! Para los que no, que sepáis que estaréis en nuestros corazones y os echaremos de menos.",
     "add_to_calendar_title": "¿Vienes? ¡Apunta las Fechas!",
     "add_prewedding_btn": "Añadir Preboda al Calendario",
     "add_wedding_btn": "Añadir Boda al Calendario"
@@ -17,8 +17,9 @@ Object.assign(translations.es, {
 Object.assign(translations.de, {
     "rsvp_page_title": "RSVP",
     "thank_you_title": "Danke!",
-    "thank_you_message": "Vielen Dank für deine Rückmeldung! Wir freuen uns riesig darauf, unseren großen Tag mit unseren Lieblingsmenschen zu feiern. Für diejenigen, die dabei sein können: Wir zählen schon die Tage! Für diejenigen, die nicht kommen können: Ihr werdet in unseren Herzen sein und wir werden euch sehr vermissen.",
-    "add_to_calendar_title": "Du kommst? Speicher die Termine!",
+    "thank_you_message": "Vielen Dank für eure Rückmeldung! Wir freuen uns riesig darauf, unseren großen Tag mit unseren Lieblingsmenschen zu feiern. Für diejenigen, die dabei sein können: Wir zählen schon die Tage! Für diejenigen, die nicht kommen können: Ihr werdet in unseren Herzen sein und wir werden euch sehr vermissen.",
+    "add_to_calendar_title": "Ihr kommt? Speichert die Termine!",
     "add_prewedding_btn": "Get-Together zum Kalender hinzufügen",
     "add_wedding_btn": "Hochzeit zum Kalender hinzufügen"
 });
+
