c319a6072178daa5b7056ffe8ceb739a26b23b1c
Juanma Olaizola <39867258+Olaizola13@users.noreply.github.com>
2025-09-20 21:22:07 +0200

    Update memories.js

--- js/translations/memories.js
+++ js/translations/memories.js
@@ -1,25 +1,24 @@
 Object.assign(translations.en, {
     "memories_page_title": "Share Your Memories",
-    "memories_intro": "One of the greatest gifts you can give us is to share the wonderful moments you capture! We've created shared albums where you can easily upload your photos and videos from both the Get-Together and the Wedding day.",
-    "memories_note": "Please choose the album that best suits your device. A Google account is required for the Google Photos album.",
+    "memories_intro": "One of the greatest gifts you can give us is to share the wonderful moments you capture! We've created a shared album where you can easily upload your photos and videos from both the Get-Together and the Wedding day.",
+    "memories_note": "A Google account is required for the Google Photos album.",
     "memories_google_button": "Google Photos Album",
     "memories_icloud_button": "iCloud Shared Album",
     "memories_thanks": "We can't wait to see the day through your eyes!"
 });
 Object.assign(translations.es, {
     "memories_page_title": "Comparte Tus Recuerdos",
-    "memories_intro": "¡Uno de los mejores regalos que podéis darnos es compartir los maravillosos momentos que capturéis! Hemos creado álbumes compartidos donde podéis subir fácilmente vuestras fotos y vídeos tanto de la preboda como del día de la boda.",
-    "memories_note": "Por favor, elegid el álbum que mejor se adapte a vuestro dispositivo. Se requiere una cuenta de Google para el álbum de Google Fotos.",
+    "memories_intro": "¡Uno de los mejores regalos que podéis darnos es compartir los maravillosos momentos que capturéis! Hemos creado un álbum compartido donde podéis subir fácilmente vuestras fotos y vídeos tanto de la preboda como del día de la boda.",
+    "memories_note": "Se requiere una cuenta de Google para el álbum de Google Fotos.",
     "memories_google_button": "Álbum de Google Fotos",
     "memories_icloud_button": "Álbum Compartido de iCloud",
     "memories_thanks": "¡Estamos deseando ver el día a través de vuestros ojos!"
 });
 Object.assign(translations.de, {
     "memories_page_title": "Teile deine Erinnerungen",
-    "memories_intro": "Eines der größten Geschenke, die ihr uns machen könnt, ist, die wundervollen Momente zu teilen, die ihr festhaltet! Wir haben gemeinsame Alben erstellt, in die ihr ganz einfach eure Fotos und Videos vom Get-Together und vom Hochzeitstag hochladen könnt.",
-    "memories_note": "Bitte wählt das Album, das am besten zu eurem Gerät passt. Für das Google Fotos-Album ist ein Google-Konto erforderlich.",
+    "memories_intro": "Eines der größten Geschenke, die ihr uns machen könnt, ist, die wundervollen Momente zu teilen, die ihr festhaltet! Wir haben ein gemeinsames Album erstellt, in das ihr ganz einfach eure Fotos und Videos vom Get-Together und vom Hochzeitstag hochladen könnt.",
+    "memories_note": "Für das Google Fotos-Album ist ein Google-Konto erforderlich.",
     "memories_google_button": "Google Fotos Album",
     "memories_icloud_button": "Geteiltes iCloud Album",
     "memories_thanks": "Wir können es kaum erwarten, den Tag durch eure Augen zu sehen!"
 });
-
