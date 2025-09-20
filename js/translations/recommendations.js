494ebf2e3022e9d6e3fbbf1262187ca4e1b48180
Juanma Olaizola <39867258+Olaizola13@users.noreply.github.com>
2025-09-20 20:49:40 +0200

    Update recommendations.js

--- js/translations/recommendations.js
+++ js/translations/recommendations.js
@@ -1,10 +1,10 @@
 Object.assign(translations.en, {
     "recommendations_page_title": "Recommendations",
     "recommendations_section1_title": "What Valladolid Has to Offer",
-    "recommendations_intro_text": "Valladolid and the wider region of Castile and León are full of history, incredible food, and wonderful sights. Below is an index to help you explore some of our favorite things about the area.",
+    "recommendations_intro_text": "Valladolid and the region of Castile and León are rich in history, delicious food and drinks, as well as historic sights. Here you will find an index to help you explore Valladolid and its surroundings.",
     "rec_index_history": "History & Culture",
     "rec_index_cathedral": "The Cathedral",
-    "rec_index_beach": "Little Caribbean", // UPDATED
+    "rec_index_beach": "Little Caribbean",
     "rec_index_storks": "The Storks",
     "rec_index_park": "Campo Grande",
     "rec_index_dayout": "Our Day Out"
@@ -12,10 +12,10 @@ Object.assign(translations.en, {
 Object.assign(translations.es, {
     "recommendations_page_title": "Recomendaciones",
     "recommendations_section1_title": "Qué Ofrece Valladolid",
-    "recommendations_intro_text": "Valladolid y la extensa región de Castilla y León están llenas de historia, una gastronomía increíble y lugares maravillosos. A continuación, un índice para ayudaros a explorar algunas de nuestras cosas favoritas de la zona.",
+    "recommendations_intro_text": "Valladolid y la región de Castilla y León son ricas en historia, deliciosa comida y bebida, así como en lugares de interés histórico. Aquí encontraréis un índice que os ayudará a explorar Valladolid y sus alrededores.",
     "rec_index_history": "Historia y Cultura",
     "rec_index_cathedral": "La Catedral",
-    "rec_index_beach": "El Pequeño Caribe", // UPDATED
+    "rec_index_beach": "El Pequeño Caribe",
     "rec_index_storks": "Las Cigüeñas",
     "rec_index_park": "Campo Grande",
     "rec_index_dayout": "Nuestro Día Perfecto"
@@ -23,10 +23,10 @@ Object.assign(translations.es, {
 Object.assign(translations.de, {
     "recommendations_page_title": "Empfehlungen",
     "recommendations_section1_title": "Was Valladolid zu bieten hat",
-    "recommendations_intro_text": "Valladolid und die weitere Region Kastilien und León sind reich an Geschichte, unglaublichem Essen und wunderschönen Sehenswürdigkeiten. Unten findet ihr ein Verzeichnis, das euch hilft, einige unserer Lieblingsdinge in der Gegend zu erkunden.",
+    "recommendations_intro_text": "Valladolid und die Region Kastilien und León sind reich an Geschichte, leckerem Essen und Getränken, sowie historischen Sehenswürdigkeiten. Hier findet ihr ein Verzeichnis, das euch bei der Erkundung Valladolids und Umgebung unterstützt.",
     "rec_index_history": "Geschichte & Kultur",
     "rec_index_cathedral": "Die Kathedrale",
-    "rec_index_beach": "Kleine Karibik", // UPDATED
+    "rec_index_beach": "Kleine Karibik",
     "rec_index_storks": "Die Störche",
     "rec_index_park": "Campo Grande",
     "rec_index_dayout": "Unser perfekter Tag"
