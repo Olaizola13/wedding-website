e5d3afc6e94ba8cb0335ccc8e688313a0670a831
Juanma Olaizola <39867258+Olaizola13@users.noreply.github.com>
2025-09-20 21:57:03 +0200

    Update rec_dayout.js

--- js/translations/rec_dayout.js
+++ js/translations/rec_dayout.js
@@ -1,6 +1,6 @@
 Object.assign(translations.en, {
     "rec_dayout_page_title": "Our Perfect Day Out",
-    "recommendations_dayout_text": "When we're in town, we love to hit the Mercado del Val, a beautiful iron market where you can grab a beer or a wine with a delicious tapa. If we don't feel like a big dinner but rather various small bites, we continue from here to <em>Calle de la Reina</em>. Our essential stops here are <em>Bar Zamora</em> (the tapa selection is incomparable) and <em>El Corcho</em> for a life-changing <em>croqueta</em>. Then it's on across the <em>Plaza Mayor</em> for dessert - ice cream from the ice cream parlor <em>Manuel Iborra</em>! For those with more time, the area around the Cathedral and the old <em>La Antigua</em> church is a beautiful place to explore.",
+    "recommendations_dayout_text": "When we're in town, we love to hit the <em>Mercado del Val<em>, a beautiful iron market where you can grab a beer or a wine with a delicious tapa. If we don't feel like a big dinner but rather various small bites, we continue from here to <em>Calle de la Reina</em>. Our essential stops here are <em>Bar Zamora</em> (the tapa selection is incomparable) and <em>El Corcho</em> for a life-changing <em>croqueta</em>. Then it's on across the <em>Plaza Mayor</em> for dessert - ice cream from the ice cream parlor <em>Manuel Iborra</em>! For those with more time, the area around the Cathedral and the old <em>La Antigua</em> church is a beautiful place to explore.",
     "dayout_map_title": "An Interactive Map of Our Route"
 });
 Object.assign(translations.es, {
@@ -10,7 +10,7 @@ Object.assign(translations.es, {
 });
 Object.assign(translations.de, {
     "rec_dayout_page_title": "Unser perfekter Tag",
-    "recommendations_dayout_text": "Wenn wir in der Stadt sind, lieben wir es, zum Mercado del Val zu gehen, einem wunderschönen Eisenmarkt, ideal für ein Bier oder einen Wein mit einer köstlichen Tapa. Wenn wir keine Lust auf ein großes Abendessen sondern auf verschiedene Kleinigkeiten haben, dann gehen wir von hier weiter in die <em>Calle de la Reina</em>. Hier sind unsere unverzichtbaren Stops die <em>Bar Zamora</em> (die Tapasauswahl ist unvergleichlich) und die <em>Bar El Corcho</em> für eine lebensverändernde <em>Croqueta</em>. Weiter geht es über die <em>Plaza Mayor</em> zum Dessert - Eis aus dem Eiscafé <em>Manuel Iborra</em>! Für diejenigen mit mehr Zeit ist die Gegend um die Kathedrale und die alte Kirche <em>La Antigua</em> ein wunderschöner Ort zum Erkunden.",
+    "recommendations_dayout_text": "Wenn wir in der Stadt sind, lieben wir es, zum <em>Mercado del Val<em> zu gehen, einem wunderschönen Eisenmarkt, ideal für ein Bier oder einen Wein mit einer köstlichen Tapa. Wenn wir keine Lust auf ein großes Abendessen sondern auf verschiedene Kleinigkeiten haben, dann gehen wir von hier weiter in die <em>Calle de la Reina</em>. Hier sind unsere unverzichtbaren Stops die <em>Bar Zamora</em> (die Tapasauswahl ist unvergleichlich) und die <em>Bar El Corcho</em> für eine lebensverändernde <em>Croqueta</em>. Weiter geht es über die <em>Plaza Mayor</em> zum Dessert - Eis aus dem Eiscafé <em>Manuel Iborra</em>! Für diejenigen mit mehr Zeit ist die Gegend um die Kathedrale und die alte Kirche <em>La Antigua</em> ein wunderschöner Ort zum Erkunden.",
     "dayout_map_title": "Eine interaktive Karte unserer Route"
 });
 
