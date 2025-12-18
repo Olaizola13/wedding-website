8910d152afa93ca0cfc3beb62840a6d4a8b47be7
Olaizola13 <olaizola.arranz@gmail.com>
2025-12-18 22:56:21 +0100

    feat(content): update rec_dayout with comprehensive guide, links, and map pins content

--- js/translations/rec_dayout.js
+++ js/translations/rec_dayout.js
@@ -1,15 +1,56 @@
+const translations = {
+    en: {},
+    es: {},
+    de: {}
+};
+
 Object.assign(translations.en, {
-    "rec_dayout_page_title": "Our Perfect Day Out",
-    "recommendations_dayout_text": "When we're in town, we love to go to the <em>Mercado del Val</em>, a historic market hall made entirely of iron. It's ideal for watching the locals shop in the morning while enjoying a <em>café con leche</em> in one of the gastro bars. In the early evening, when the market stalls close, people meet for a drink or two, which is always accompanied by a free <em>tapa</em> (small appetizer) of your choice. For dinner afterwards, we can recommend the restaurants in the adjacent <em>Calle del Conde Ansurez</em>. Those who can still stand will arrive in the area around the Cathedral and the old church \"La Antigua\" after a 7-minute walk and can look for a bar for another drink with a tapa.",
-    "dayout_map_title": "An Interactive Map of Our Route"
+    "rec_dayout_page_title": "Our Perfect Day",
+    "dayout_intro_market": "One of our typical starting points in Valladolid is the <a href='/rec_gastronomy.html#hotspots'>Mercado del Val</a>, the city's historic market hall. In the morning, you can watch the locals shopping and chatting while enjoying a <em>café con leche</em> and a sweet treat (e.g., <em>Mantecados de Almendra</em> or <em>Bombones</em>) at one of the gastrobars inside.",
+    "dayout_afternoon_drinks": "In the late afternoon / early evening, when the market stalls close, people meet for a <a href='/rec_gastronomy.html#drinks'>drink</a> or two, which is automatically accompanied by a tapa of your choice at no extra cost.",
+    "dayout_dinner": "For dinner afterwards, we can recommend restaurants in the adjacent <a href='https://www.google.com/maps/search/Calle+de+Francisco+Zarandona,+Valladolid' target='_blank'>Calle de Francisco Zarandona</a> (e.g., <a href='https://matamales.es/' target='_blank'>Matamales</a>) or <a href='https://www.google.com/maps/search/Calle+Sandoval,+Valladolid' target='_blank'>Calle Sandoval</a>.",
+    "dayout_tipping": "<strong>Tipping:</strong> Can be given, but is not expected in gastrobars. In restaurants, a tip of 5-10% of the bill is customary.",
+    "dayout_alternatives_title": "Alternatives to Restaurant Dinner",
+    "dayout_alternatives_text": "Highly recommended alternatives to a restaurant dinner can be found in Calle de la Reina / Calle Correos. <a href='https://www.google.com/maps/place/Bar+Zamora,+C.+Correos,+5,+47001+Valladolid,+Spain' target='_blank'>Bar Zamora</a> has the most creative and delicious tapas. You can eat and drink your way through an almost endless selection of small delicacies for very little money. A few houses further down on the other side of the street is <a href='https://www.google.com/maps/place/Bar+El+Corcho,+C.+Correos,+2,+47001+Valladolid,+Spain' target='_blank'>Bar El Corcho</a>. Here you can find the best <a href='/rec_gastronomy.html#food'>croquetas</a> in all of Valladolid.",
+    "dayout_extra_title": "Further Recommendations",
+    "dayout_extra_list_1": "The bars around the unfinished Church of Santa María de la Antigua.",
+    "dayout_extra_list_2": "The Pasaje Gutiérrez, a shopping arcade built in the late 19th century directly inspired by Parisian galleries, which today mainly houses cafés and pubs.",
+    "dayout_extra_list_3": "Drink a Gin Tonic and eat a piece of cake at <a href='https://www.google.com/maps/place/Lion+d%27Or,+Pl.+Mayor,+47001+Valladolid,+Spain' target='_blank'>Lion d'Or</a>.",
+    "dayout_extra_list_4": "Have a very good lunch or dinner at the restaurant <a href='https://www.google.com/maps/place/Restaurante+Los+Ilustres,+C.+Cascajares,+2,+47002+Valladolid,+Spain' target='_blank'>Los Ilustres</a>.",
+    "dayout_extra_list_5": "Visit the Plaza Mayor and then grab an ice cream or Turrones from <a href='https://www.google.com/maps/place/Turrones+Manuel+Iborra,+C.+Lencer%C3%ADa,+2,+47001+Valladolid,+Spain' target='_blank'>Manuel Iborra</a> in Calle Lencería.",
+    "dayout_map_title": "Interactive Map"
 });
+
 Object.assign(translations.es, {
     "rec_dayout_page_title": "Nuestro Día Perfecto",
-    "recommendations_dayout_text": "Cuando estamos en la ciudad, nos encanta ir al <em>Mercado del Val</em>, una histórica lonja de mercado construida enteramente en hierro. Es ideal para ver a los locales hacer sus compras por la mañana mientras se disfruta de un café con leche en uno de los gastrobares. A primera hora de la tarde, cuando cierran los puestos, la gente se reúne para tomar algo, que siempre viene acompañado de una tapa gratis (pequeño aperitivo) a elegir. Para cenar después, podemos recomendar los restaurantes de la adyacente <em>Calle del Conde Ansurez</em>. Quienes todavía se tengan en pie, llegarán a la zona de la Catedral y la antigua iglesia de <em>La Antigua</em> tras 7 minutos a pie y podrán buscar un bar para tomar otra copa con una tapa.",
-    "dayout_map_title": "Un Mapa Interactivo de Nuestra Ruta"
+    "dayout_intro_market": "Uno de nuestros puntos de partida típicos en Valladolid es el <a href='/rec_gastronomy.html#hotspots'>Mercado del Val</a>, el histórico mercado de la ciudad. Por la mañana, puedes ver a los lugareños comprando y charlando mientras disfrutas de un <em>café con leche</em> y un dulce (por ejemplo, <em>Mantecados de Almendra</em> o <em>Bombones</em>) en uno de los gastrobares del interior.",
+    "dayout_afternoon_drinks": "A última hora de la tarde / primera hora de la noche, cuando cierran los puestos del mercado, la gente se reúne para tomar una <a href='/rec_gastronomy.html#drinks'>bebida</a> o dos, que se acompaña automáticamente de una tapa a elegir sin coste adicional.",
+    "dayout_dinner": "Para cenar después, recomendamos restaurantes en la adyacente <a href='https://www.google.com/maps/search/Calle+de+Francisco+Zarandona,+Valladolid' target='_blank'>Calle de Francisco Zarandona</a> (por ejemplo, <a href='https://matamales.es/' target='_blank'>Matamales</a>) o <a href='https://www.google.com/maps/search/Calle+Sandoval,+Valladolid' target='_blank'>Calle Sandoval</a>.",
+    "dayout_tipping": "<strong>Propinas:</strong> Se pueden dar, pero no se esperan en los gastrobares. En los restaurantes, es habitual dejar una propina del 5-10% de la cuenta.",
+    "dayout_alternatives_title": "Alternativas a la Cena en Restaurante",
+    "dayout_alternatives_text": "En la Calle de la Reina / Calle Correos encontraréis alternativas muy recomendables a una cena en restaurante. El <a href='https://www.google.com/maps/place/Bar+Zamora,+C.+Correos,+5,+47001+Valladolid,+Spain' target='_blank'>Bar Zamora</a> tiene las tapas más creativas y deliciosas. Aquí se puede comer y beber a base de una selección casi interminable de pequeños manjares por muy poco dinero. Unas casas más abajo, al otro lado de la calle, se encuentra el <a href='https://www.google.com/maps/place/Bar+El+Corcho,+C.+Correos,+2,+47001+Valladolid,+Spain' target='_blank'>Bar El Corcho</a>. Aquí se encuentran las mejores <a href='/rec_gastronomy.html#food'>croquetas</a> de todo Valladolid.",
+    "dayout_extra_title": "Otras Recomendaciones",
+    "dayout_extra_list_1": "Los bares alrededor de la inacabada Iglesia de Santa María de la Antigua.",
+    "dayout_extra_list_2": "El Pasaje Gutiérrez, una galería comercial construida a finales del siglo XIX inspirada en las galerías parisinas, que hoy alberga principalmente cafés y pubs.",
+    "dayout_extra_list_3": "Tomar un Gin Tonic y comer un trozo de tarta en el <a href='https://www.google.com/maps/place/Lion+d%27Or,+Pl.+Mayor,+47001+Valladolid,+Spain' target='_blank'>Lion d'Or</a>.",
+    "dayout_extra_list_4": "Comer o cenar muy bien en el restaurante <a href='https://www.google.com/maps/place/Restaurante+Los+Ilustres,+C.+Cascajares,+2,+47002+Valladolid,+Spain' target='_blank'>Los Ilustres</a>.",
+    "dayout_extra_list_5": "Visitar la Plaza Mayor y luego tomar un helado o turrones de <a href='https://www.google.com/maps/place/Turrones+Manuel+Iborra,+C.+Lencer%C3%ADa,+2,+47001+Valladolid,+Spain' target='_blank'>Manuel Iborra</a> en la Calle Lencería.",
+    "dayout_map_title": "Mapa Interactivo"
 });
+
 Object.assign(translations.de, {
     "rec_dayout_page_title": "Unser perfekter Tag",
-    "recommendations_dayout_text": "Wenn wir in der Stadt sind, lieben wir es, zum <em>Mercado del Val</em> zu gehen, einer historischen Markthalle, die vollständig aus Eisen errichtet ist. Ideal, um am Vormittag den Einheimischen beim Einkauf zuzusehen, während man in den Gastrobars einen <em>Café con leche</em> trinkt. Am frühen Abend, wenn die Marktstände schließen, trifft man sich auf das ein oder andere Getränk, welches immer von einer kostenlosen <em>Tapa</em> (kleines Appetithäppchen) eurer Wahl begleitet wird. Zum Abendessen danach können wir die Restaurants in der angrenzenden <em>Calle del Conde Ansurez</em> empfehlen. Wer noch stehen kann, der trifft nach 7min Fußweg in der Gegend um die Kathedrale und die alte Kirche \"La Antigua\" ein und sucht sich eine Bar für ein weiteres Getränk mit einer Tapa.",
-    "dayout_map_title": "Eine interaktive Karte unserer Route"
+    "dayout_intro_market": "Einer unserer typischen Anlaufpunkte in Valladolid ist der <a href='/rec_gastronomy.html#hotspots'>Mercado del Val</a>, die historische Markthalle der Stadt. Am Vormittag kann man dort in einer der Gastrobars im Markt bei einem <em>café con leche</em> und einem \"Teilchen\" (z.B. <em>Mantecados de Almendra</em> oder <em>Bombones</em>) die Einheimischen beim Einkaufen und Plaudern beobachten.",
+    "dayout_afternoon_drinks": "Am späten Nachmittag / frühen Abend, wenn die Marktstände schließen, trifft man sich auf das ein oder andere <a href='/rec_gastronomy.html#drinks'>Getränk</a>, welches automatisch und ohne weitere Kosten mit einer Tapa eurer Wahl begleitet wird.",
+    "dayout_dinner": "Zum Abendessen danach können wir dir Restaurants in der angrenzenden <a href='https://www.google.com/maps/search/Calle+de+Francisco+Zarandona,+Valladolid' target='_blank'>Calle de Francisco Zarandona</a> (z.B. <a href='https://matamales.es/' target='_blank'>Matamales</a>) oder <a href='https://www.google.com/maps/search/Calle+Sandoval,+Valladolid' target='_blank'>Calle Sandoval</a> empfehlen.",
+    "dayout_tipping": "<strong>Trinkgeld:</strong> Kann gegeben werden, wird aber in Gastrobars nicht erwartet. In Restaurants ist ein Trinkgeld von 5-10% des Rechnungspreises üblich.",
+    "dayout_alternatives_title": "Alternativen zum Restaurant",
+    "dayout_alternatives_text": "Sehr empfehlenswerte Alternativen zu einem Abendessen im Restaurant findet ihr in der Calle de la Reina / Calle Correos. Die <a href='https://www.google.com/maps/place/Bar+Zamora,+C.+Correos,+5,+47001+Valladolid,+Spain' target='_blank'>Bar Zamora</a> hat die kreativsten und leckersten Tapas. Für kleines Geld isst und trinkt man sich hier durch eine schier endlose Auswahl an Kleinigkeiten. Ein paar Häuser weiter auf der anderen Straßenseite befindet sich die <a href='https://www.google.com/maps/place/Bar+El+Corcho,+C.+Correos,+2,+47001+Valladolid,+Spain' target='_blank'>Bar El Corcho</a>. Hier gibt es die besten <a href='/rec_gastronomy.html#food'>Croquetas</a> in ganz Valladolid.",
+    "dayout_extra_title": "Weitere Empfehlungen",
+    "dayout_extra_list_1": "Die Bars rund um die unvollendete Iglesia de Santa María de la Antigua.",
+    "dayout_extra_list_2": "Die Pasaje Gutiérrez, eine im späten 19. Jahrhundert nach Pariser Vorbild errichtete Einkaufspassage, die heute - wie sollte es anders sein - vor allem Cafés und Kneipen beherbergt.",
+    "dayout_extra_list_3": "Einen Gin Tonic trinken und dazu ein Stück Kuchen essen im <a href='https://www.google.com/maps/place/Lion+d%27Or,+Pl.+Mayor,+47001+Valladolid,+Spain' target='_blank'>Lion d'Or</a>.",
+    "dayout_extra_list_4": "Sehr gut zu Mittag oder zu Abend essen im Restaurant <a href='https://www.google.com/maps/place/Restaurante+Los+Ilustres,+C.+Cascajares,+2,+47002+Valladolid,+Spain' target='_blank'>Los Ilustres</a>.",
+    "dayout_extra_list_5": "Die Plaza Mayor anschauen und anschließend ein Eis oder Turrones von <a href='https://www.google.com/maps/place/Turrones+Manuel+Iborra,+C.+Lencer%C3%ADa,+2,+47001+Valladolid,+Spain' target='_blank'>Manuel Iborra</a> in der Calle Lencería mitnehmen.",
+    "dayout_map_title": "Interaktive Karte"
 });
